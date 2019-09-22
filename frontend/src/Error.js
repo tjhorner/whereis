4a18f4565bc2c8b3772134dfa58fdd8a0ba0692c
TJ Horner <me@tjhorner.com>
2019-09-22 16:44:28 -0400

    attribution

--- frontend/src/Error.js
+++ frontend/src/Error.js
@@ -5,7 +5,11 @@ import { Link } from "react-router-dom"
 
 const Error = props => (
   <div className="error">
-    <img src={hyena}/>
+    <figure>
+      <img src={hyena}/>
+      <figcaption>(Hyena sticker from <a href="https://store.line.me/stickershop/product/4439542/en" target="_blank" rel="noopener noreferrer">this pack</a>)</figcaption>
+    </figure>
+
     <h1>Something went wrong.</h1>
     <h4>{props.message}</h4>
     {window.location.pathname !== "/" && <Link to="/">Go back</Link>}
