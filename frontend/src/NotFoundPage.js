18e0069db0e46838c1cfc0040136d6532438bb5b
TJ Horner <me@tjhorner.com>
2019-09-22 15:30:37 -0400

    Ye

--- frontend/src/NotFoundPage.js
+++ frontend/src/NotFoundPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Error from './Error'
 
-const NotFoundPage = props => <Error message="Not Found"/>
+const NotFoundPage = props => <Error message="This page couldn't be found."/>
 
 export default NotFoundPage
